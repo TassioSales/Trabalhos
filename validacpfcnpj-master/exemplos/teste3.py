f4f90552caf84087d30d2809c848bdddb2eba28d
TassioSales <74218122+TassioSales@users.noreply.github.com>
2021-03-26 20:47:05 -0300

    teste

---
+++ validacpfcnpj-master/exemplos/teste3.py
@@ -0,0 +1,29 @@
+# Apenas alguns testes, por favor, ignore
+import sys
+import os.path
+
+sys.path.append(
+    os.path.abspath(os.path.join(os.path.dirname(__file__), os.path.pardir)))
+
+import validacpfcnpj
+
+cpf_cnpj = validacpfcnpj.ValidaCpfCnpj()
+
+arquivo = open("estagiario.txt", "r")
+for item in arquivo:
+    cpf_cnpj.cpf_cnpj = item
+    if len(cpf_cnpj.cpf_cnpj) >= 14:
+        if cpf_cnpj.valida():
+            print(f'CNPJ {cpf_cnpj.cpf_cnpj} Válido.')
+        else:
+            print(f'CNPJ {cpf_cnpj.cpf_cnpj} Invalido.')
+    if len(cpf_cnpj.cpf_cnpj) < 14:
+        cpf_cnpj.cpf_cnpj = item
+        if cpf_cnpj.valida():
+            print(f'CPF {cpf_cnpj.cpf_cnpj} Válido.')
+        else:
+            print(f'CPF {cpf_cnpj.cpf_cnpj} Invalido.')
+
+arquivo.close()
+
+
